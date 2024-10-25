9498678ceb2195d7be502d18ec612ed823bc8e42
Pipsson <juliusmallya8420@gmail.com>
2024-10-25 11:58:48 +0300

    feat(page.tsx):handling data fetchng from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Results from "../app/components/Results"; // Adjust the path as necessary
+const API_KEY = process.env.API_KEY;
+interface SearchParams {
+  genre?: string;
+}
 
-function Homepage() {
-  return <div>Homepage</div>;
+async function Homepage({ searchParams }: { searchParams: SearchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? `movie/top_rated ` : `trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+  console.log(results);
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
 
 export default Homepage;
